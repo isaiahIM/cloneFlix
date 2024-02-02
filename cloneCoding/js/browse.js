d426d949613bf2974d8d0a317e35fc2fb3b194ea
jaewon926 <jjojjo0926@naver.com>
2024-02-02 13:30:41 +0900

    Slider 수정 & 이미지 호버

--- cloneCoding/js/browse.js
+++ cloneCoding/js/browse.js
@@ -1,19 +1,51 @@
-scroll(function(){
-    var x = document.getElementsByClassName("sliders")[0];
-    var step = window.outerWidth / 2;
-    x.scrollLeft += step;
+new Swiper(".slider1 .swiper-container", {
+    loop: true,
+    slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+
+    
+    navigation: {
+        // 슬라이드 이전/다음 버튼 사용 여부
+        nextEl: ".slider1 .swiper-next", // 다음 버튼 선택자
+    },  
 });
 
-getScrollVal(function() {
-    var x = document.getElementsByClassName("content")[0];
+new Swiper(".slider2 .swiper-container", {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 10,
+    
+    navigation: {
+        nextEl: ".slider2 .swiper-next",
+    },  
+});
 
-    var el2 = document.getElementsByClassName("right_arrow")[0];
-    var right = x.scrollWidth - (x.scrollLeft + x.clientWidth) + 1;
-    if (right <= 2) {
-        el2.style.display = "none";
-    } else {
-        el2.style.display = "flex";
-    }
-}, 550);
+new Swiper(".slider3 .swiper-container", {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 10,
+    
+    navigation: {
+        nextEl: ".slider3 .swiper-next",
+    },  
+});
 
-getScrollVal();
+new Swiper(".slider4 .swiper-container", {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 10,
+    
+    navigation: {
+        nextEl: ".slider4 .swiper-next",
+    },  
+});
+
+new Swiper(".slider5 .swiper-container", {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 10,
+    
+    navigation: {
+        nextEl: ".slider5 .swiper-next",
+    },  
+}); 
